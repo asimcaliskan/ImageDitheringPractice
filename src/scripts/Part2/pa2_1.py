435f9fb97b5abfc67e7e4347ce55e8055c8ddc25
Asım <ismailasimcaliskan@gmail.com>
2021-11-09 20:43:35 +0300

    finished

--- src/scripts/Part2/pa2_1.py
+++ src/scripts/Part2/pa2_1.py
@@ -1,17 +1,59 @@
-from numpy.lib.utils import source
 from pa2_2 import color_transfer
 import cv2
+from PIL import Image 
+import numpy as np
+import math
 
 SOURCE_IMAGE_PATH = "C:\\Users\\MONSTER\\GitHub\\ImageDitheringPractice\\exampleImages\\colortransfer\\ocean_day.jpg"
-TARGET_IMAGE_PATH = "C:\\Users\\MONSTER\\GitHub\\ImageDitheringPractice\\exampleImages\\colortransfer\\autumn.jpg"
+TARGET_IMAGE_PATH = "C:\\Users\\MONSTER\\GitHub\\ImageDitheringPractice\\exampleImages\\colortransfer\\ocean_sunset.jpg"
+
+
+def RGB2LMS(image):
+	converter_matrix = np.array( [[0.3811, 0.5783, 0.0402], [0.1967, 0.7244, 0.0782], [0.0241, 0.1288, 0.844]])
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+			image[row][column] = np.matmul(converter_matrix, image[row, column])
+
+def LMS2LAB(image):
+	matrix_1 = np.array(
+		[[1, 1, 1],
+		[1, 1, -2],
+		[1, -1, 0]])
+	matrix_2 = np.array(
+		[[1/math.sqrt(3), 0, 0],
+		[0, 1/math.sqrt(6), 0],
+		[0, 0, 1/math.sqrt(2)]])
+	converter_matrix = np.matmul(matrix_2, matrix_1)
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+			image[row][column] = np.matmul(converter_matrix, image[row, column])
+
+def LMS2LOG10LMS(image):
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+			for chnnl in range(channel):
+				if image[row][column][chnnl] > 0.: 
+					image[row][column][chnnl] = np.log10(image[row][column][chnnl])
+
 
 def read_image():
-	source_image = cv2.imread(SOURCE_IMAGE_PATH)
-	source_image = cv2.cvtColor(source_image,cv2.COLOR_BGR2LAB)
-	target_image = cv2.imread(TARGET_IMAGE_PATH)
-	target_image = cv2.cvtColor(target_image,cv2.COLOR_BGR2LAB)
+	source_image = Image.open(SOURCE_IMAGE_PATH)
+	source_image = np.array(source_image)
+	source_image = source_image.astype(float)
+	RGB2LMS(source_image)
+	LMS2LOG10LMS(source_image)
+	LMS2LAB(source_image)
+	target_image = Image.open(TARGET_IMAGE_PATH)
+	target_image = np.array(target_image)
+	target_image = target_image.astype(float)
+	RGB2LMS(target_image)
+	LMS2LOG10LMS(target_image)
+	LMS2LAB(target_image)
 	return source_image, target_image
 
-source_image, target_image = read_image()
 
+source_image, target_image = read_image()
 color_transfer(source_image, target_image)
--- src/scripts/Part2/pa2_2.py
+++ src/scripts/Part2/pa2_2.py
@@ -1,43 +1,77 @@
 import numpy as np
 import cv2
+import math
+from PIL import Image
+def LMS2RGB(image):
+	converter_matrix = np.array( [[4.4679, -3.5783, 0.1193], [-1.2186, 2.3809, -0.1624], [0.0497, -0.2439, 1.2045]])
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+			image[row][column] = np.matmul(converter_matrix, image[row, column])
+
+def LAB2LMS(image):
+	matrix_1 = np.array(
+		[[1, 1, 1],
+		[1, 1, -1],
+		[1, -2, 0]])
+	matrix_2 = np.array(
+		[[1/math.sqrt(3), 0, 0],
+		[0, 1/math.sqrt(6), 0],
+		[0, 0, 1/math.sqrt(2)]])
+	converter_matrix = np.matmul(matrix_1, matrix_2)
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+			image[row][column] = np.matmul(converter_matrix, image[row, column])
+
+def LOG10LMS2LMS(image):
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+				image[row][column] = np.power(10, image[row][column])
+
 
 def calculate_mean_and_standart_deviation(image):
 	image_mean, image_std = cv2.meanStdDev(image)
-	image_mean = np.hstack(np.around(image_mean, 3))
-	image_std = np.hstack(np.around(image_std ,3))
+	image_mean = np.hstack(image_mean)
+	image_std = np.hstack(image_std)
     #image_mean : [mean_of_channel_1, mean_of_channel_2, mean_of_channel_3]
     #image_std : [std_of_channel_1, std_of_channel_2, std_of_channel_3]
 	return image_mean, image_std
 
 #0 =< image channel value =< 255
-def pixel_boundary_check(pixel):
-    if(pixel < 0): 
-        return 0
-    elif(pixel > 255):
-        return 255
-    else:
-        return pixel
+def pixel_boundary_check(image):
+	height, width, channel = image.shape
+	for row in range(height):
+		for column in range(width):
+			for chnnl in range(channel):
+				if(image[row][column][chnnl] < 0): 
+					image[row][column][chnnl] = 0
+				elif(image[row][column][chnnl] > 255):
+					image[row][column][chnnl] = 255
+
 
 def color_transfer(source_image, target_image):
-    source_image_mean, source_image_std = calculate_mean_and_standart_deviation(source_image)
-    target_image_mean, target_image_std = calculate_mean_and_standart_deviation(target_image)
+	source_image_mean, source_image_std = calculate_mean_and_standart_deviation(source_image)
+	target_image_mean, target_image_std = calculate_mean_and_standart_deviation(target_image)
 
-    source_image_height, source_image_width, source_image_channel = source_image.shape
-    for row in range(0,source_image_height):
-        for column in range(0,source_image_width):
-            for channel in range(0,source_image_channel):
-             
-                pixel = source_image[row, column, channel]
+	source_image_height, source_image_width, source_image_channel = source_image.shape
+	for row in range(0,source_image_height):
+		for column in range(0,source_image_width):
+			for channel in range(0,source_image_channel):
+				pixel = source_image[row, column, channel]
                 #step 5, 6 and 7 on the paper
                 #l'" = (var_lt/var_ls).(l − mean_ls) + mean_lt
                 #α'" = (var_αt/var_αs).(α − mean_αs) + mean_αt
                 #β'" = (var_βt/var_βs).(β − mean_βs) + mean_βt
-                pixel = ((target_image_std[channel] / source_image_std[channel]) * (pixel - source_image_mean[channel])) + target_image_mean[channel]
-                pixel = round(pixel)
-                pixel = pixel_boundary_check(pixel)
-                source_image[row, column, channel] = pixel
+				pixel = ((target_image_std[channel] / source_image_std[channel]) * (pixel - source_image_mean[channel])) + target_image_mean[channel]
+				source_image[row, column, channel] = pixel
 
-    source_image = cv2.cvtColor(source_image, cv2.COLOR_LAB2BGR)
-    cv2.imshow("RESULT IMAGE", source_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+	#source_image = cv2.cvtColor(source_image, cv2.COLOR_LAB2RGB)
+	LAB2LMS(source_image)
+	LOG10LMS2LMS(source_image)
+	LMS2RGB(source_image)
+	pixel_boundary_check(source_image)
+	source_image = source_image.astype(np.uint8)
+	source_image = Image.fromarray(source_image)
+	source_image.show()
